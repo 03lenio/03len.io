8911dfe899b4471f73325594e78a2b2dea9e7658
03lenio <tech@03len.io>
2025-10-14 21:31:24 +0200

    inital commit

---
+++ src/lib/components/TitleComp.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    
+    let tooltip: HTMLDivElement;
+
+    function toggleDarkMode() {
+        const isDarkMode = document.documentElement.classList.toggle('dark');
+        if (isDarkMode) {
+            tooltip.textContent = 'Go Light';
+        } else {
+            tooltip.textContent = 'Go Dark';
+        }
+        localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+    }
+    
+    // Apply the saved theme on page load
+    onMount(() => {
+        const savedMode = localStorage.getItem('darkMode');
+        if (savedMode === 'enabled') {
+            document.documentElement.classList.add('dark');
+            tooltip.textContent = 'Go Light';
+        }
+    });
+    </script>
+    
+    <div class="title-container flex flex-col cursor-pointer mb-[6.5em] relative group">
+        <!-- Outer stretched text -->
+        <span class="font-outer text-[5em] font-times text-black dark:text-white scale-y-[2.7] leading-none block text-center">
+          03LENIO
+        </span>
+        <!-- Inner normal text -->
+        <span class="font-inner absolute inset-0 flex items-center justify-center text-[5em] font-arial text-black dark:text-white scale-[0.95,0.7] leading-none">
+          03LENIO
+        </span>
+        <!-- Tooltip -->
+        <div onclick={toggleDarkMode} bind:this={tooltip} class="tooltiptext absolute bottom-[-4em] text-black dark:text-white left-0 w-[5em] opacity-0 bg-transparent font-mono text-center p-[5px_0] rounded transition-opacity duration-300 ease-in-out z-[1] group-hover:opacity-100">
+          Go Dark
+        </div>
+    </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import TitleComp from "$lib/components/TitleComp.svelte";
+
+</script>
+
+
+
+<div class="flex items-center justify-center h-screen dark:bg-black transition-colors duration-500">
+    <TitleComp />
+</div>
